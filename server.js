567d5b845cbb22613461a3f91664816ba96a4ef3
ahsansadman <sadmannafim123@gmail.com>
2019-11-07 08:14:38 +0600

    CLASSWORK 8C

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 var fs = require('fs')
   var server = http.createServer(function(req, res){
     
@@ -16,3 +16,39 @@ var fs = require('fs')
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
   });
+*/
+
+
+
+const http = require('http');
+const app = require('express')();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+
+app.get('/second', function(req, res){
+	res.sendFile(__dirname+'/second.html');
+});
+
+app.get('/new_article', function(req, res){
+	res.sendFile(__dirname+'/new_article.html');
+});
+
+
+app.post('/new_article', function(req, res){
+	console.log(req.body);
+	res.send('response successfully sent');
+});
+
+
+app.listen(3000, 'localhost', function() {
+	console.log('Server running');
+});
